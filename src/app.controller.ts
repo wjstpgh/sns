dd08ddc875882f931ea18dc65018f639015c56af
jake_전세호 <jake@bookips.com>
2024-12-23 17:36:27 +0900

    feat: json post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('post')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스_민지',
+      content: '메이크업 고치는 민지',
+      likeCount: 10000000,
+      commentCount: 9999,
+    };
   }
 }
